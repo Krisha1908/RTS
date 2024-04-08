1bc0bb99a2cdc626dc632114a6c0302cbb7aebcb
Krisha1908 <krishalavingiya19@gmail.com>
2024-04-08 17:37:43 +0530

    ....

--- Javascript/Promise/stopWatch.js
+++ Javascript/Promise/stopWatch.js
@@ -1,8 +1,14 @@
 function start()
 {
+   var start = document.getElementById("start");
+   var reset = document.getElementById("reset");
+   
+   reset.addEventListener("click" , () =>{
     
+   })
     var time = hour + ":" + mins + ":" + sec;
     document.getElementById("txt").innerHTML = time;
+    
 }
 
 setInterval(start,1000);
