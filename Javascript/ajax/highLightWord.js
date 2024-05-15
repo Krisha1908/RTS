590e152be8733d968b5e2a62e9c404e71dee2b61
Krisha1908 <krishalavingiya19@gmail.com>
2024-05-15 14:03:20 +0530

    ....

--- Javascript/ajax/highLightWord.js
+++ Javascript/ajax/highLightWord.js
@@ -1,19 +1,46 @@
 function highlightWord() {
     var sentence = document.getElementById("sentence").textContent;
-    var textmark = document.getElementById("textMark").value.toUpperCase(); 
+    var textmark = document.getElementById("textMark").value; 
     var highlightedSentence = "";
 
     for (var i = 0; i < sentence.length; i++) {
-        if (sentence[i] === textmark[i])
+        if (sentence[i] == textmark[i]) 
         {
             highlightedSentence += '<span class="highlight">' + sentence[i] + '</span>';
         } 
-        else
+        else 
         {
-            highlightedSentence =  highlightedSentence + sentence[i]; 
+            highlightedSentence += sentence[i]; 
         }
     }
 
-    var sentence = document.getElementById("sentence");
-    sentence.innerHTML = highlightedSentence;
+    var sentenceElem = document.getElementById("sentence");
+    sentenceElem.innerHTML = highlightedSentence;  
+    
+    
+    if(count==0)
+    {
+        startTime = performance.now();
+        console.log(startTime);
+        var startTime = document.getElementById("startTime");
+        st.innerHTML = "StartTime = " + startTime;
+
+    }
+    count++;
+}
+
+var count = 0;
+var startTime;
+
+function end()
+{
+    var time = document.getElementById("time");
+    var endTime = performance.now();
+    console.log(end);
+    // var endTime = document.getElementById("endTime");
+    // et.innerHTML = "EndTime = " + end;
+
+    diff = (endTime - startTime)/1000;
+    console.log("Diff ",diff);
+
 }
