4c0b894105faaefddad94281e183d4de562c2d88
Krisha1908 <krishalavingiya19@gmail.com>
2024-05-21 18:29:14 +0530

    ....

---
+++ JAVA/Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+    public static void main(String args[])
+    {
+        System.out.println("HELLO WORLD");
+        System.out.println('A');
+        System.out.println(1);
+
+    }    
+}
