d018a8979e261623a769671fc160e16dfb349cf0
Krisha1908 <krishalavingiya19@gmail.com>
2024-05-27 14:25:52 +0530

    ...

---
+++ JAVA/demo2.java
@@ -0,0 +1,55 @@
+// package com.method_var;
+
+public class MethodDemo1
+{
+
+	public void test() 
+    {
+
+		System.out.println("test method called...");
+		// any non static method can be called only with class's object if we are calling from static method
+		// but if you want to call non static method from non static method no need of
+		// object...
+
+	}
+
+	public int sum(int a, int b)
+    {
+		// non static wa r with argument
+		// int c = a + b;
+		// return c;
+		return a + b;
+	}
+
+	public boolean isValid()
+     {
+		sum(100, 200);
+		return false;
+
+	}
+
+	public static void main(String[] args)
+     {
+	
+		MethodDemo1 m1 = new MethodDemo1();
+		m1.test();
+		
+		int ans = m1.sum(100, 20);
+		System.out.println("ans  = " + ans);
+
+		boolean ans1 = m1.isValid();
+		System.out.println("ans = " + ans1);
+
+		if (m1.isValid())
+        {
+			System.out.println("true...");
+		} 
+        else
+        {
+			System.out.println("false...");
+		}
+
+	}
+}
+
+class
